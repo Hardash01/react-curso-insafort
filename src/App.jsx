369cffb0de70afb004654e3ab9dec8035b8334f5
Hardash01 <edwin98santos@gmail.com>
2023-10-27 20:08:23 -0600

    Tarea de Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Carousel } from "./components/Carousel"
 import { Nabvar } from "./components/Nabvar"
 import { Footer } from "./components/Footer"
+import Header from "./components/Header"
 
 function App() {
 
@@ -8,6 +9,9 @@ function App() {
     <>
       <Nabvar />
       <Carousel />
+      <Header title="pruebas de props"/>
+      <Header title="Bosque en la noche" name="Noche en el bosque" category="imagenes de noche"/>
+      <Header title="Ciudad en la noche" name="Noche en la ciudad" category="imagenes de noche"/>
       <Footer />
     </>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+
+function Header (props) {
+
+    return (
+        <div>
+            <h1 className="text-center mb-5 text-xl font-bold">{props.title}</h1>
+            <p className="text-center">Nombre de Imagen: {props.name}</p>
+            <p className="text-center mb-10">Categoria: {props.category}</p>
+        </div>
+    )
+}
+
+Header.propTypes = {
+    title: PropTypes.string,
+    name: PropTypes.string,
+    category: PropTypes.string,
+};
+
+Header.defaultProps = {
+    title: "Titulo de la Prop",
+    name: "Nombre Predeterminado",
+    category: "Edad Predeterminada"
+}
+
+export default Header
